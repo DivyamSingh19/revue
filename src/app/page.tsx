6eeb6672e849fcb5fc24c2ec6ceba4a73ef7f282
focusedaf <126765236+focusedaf@users.noreply.github.com>
2025-08-01 13:36:17 +0530

    stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
+import { Button } from '@/components/ui/button'
 
 const page = () => {
   return (
-    <div>
+    <div className="flex gap-5 flex-col min-h-screen justify-center items-center">
       <h3>yaha page transistion aur loader effect daalenge</h3>
       <p>yaha voh layout and theme choose kareneka option denge</p>
+      <div className='flex gap-5'>
+        <Button>Select Layout</Button>
+        <Button>Select Theme</Button>
+      </div>
     </div>
-  )
+  );
 }
 
 export default page
--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,13 +1,18 @@
-import React from 'react'
+import React from "react";
+import { Button } from "@/components/ui/button";
 
 const Portfolio = () => {
   return (
-    <div>
-        <h3>image grid,ripple hover effect daalna hai</h3>
-        <h3>yahi resume upload ka option + auto fill stuff</h3>
-        <h3>download qr of portfolio ka button</h3>
+    <div className="flex gap-5 flex-col min-h-screen justify-center items-center">
+      <h3>image grid,ripple hover effect daalna hai</h3>
+      <h3>yahi resume upload ka option + auto fill stuff</h3>
+      <h3>download qr of portfolio ka button</h3>
+      <div className="flex gap-5">
+        <Button>Upload Resume</Button>
+        <Button>Download Portfolio</Button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Portfolio
+export default Portfolio;
