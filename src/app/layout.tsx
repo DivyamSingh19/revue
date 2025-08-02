d7f2e293eac8109008b2b15183366f4202f845f2
Divyam Singh <divyamsingh1918@gmail.com>
2025-08-02 06:46:50 +0530

    updates

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,11 +23,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark">
+    <html lang="en" className="dark  ">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"> 
         {children}
+        </div>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { Button } from '@/components/ui/button'
 
 const page = () => {
   return (
+    
     <div className="flex gap-5 flex-col min-h-screen justify-center items-center">
       <h3>yaha page transistion aur loader effect daalenge</h3>
       <p>yaha voh layout and theme choose kareneka option denge</p>
